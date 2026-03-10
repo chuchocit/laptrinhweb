3cc9ebe0e367a6b47e2ac895b5785a483bea7854
chuchocit <nguyenhuuchuc212gmail.com>
2026-03-10 16:54:09 +0700

    update user list

--- user_list.php
+++ user_list.php
@@ -1,5 +1,6 @@
 <?php
 require_once "db.php";
+require_once "csrf.php";
 
 $db = new Database();
 
@@ -16,14 +17,43 @@ $users = $db->select($sql);
         <th>ID</th>
         <th>Username</th>
         <th>Email</th>
+        <th>Action</th>
+        <th>Chi tiết</th>
     </tr>
 
-<?php foreach($users as $user): ?>
-    <tr>
-        <td><?php echo $user['id']; ?></td>
-        <td><?php echo $user['username']; ?></td>
-        <td><?php echo $user['email']; ?></td>
-    </tr>
+<?php foreach ($users as $user): ?>
+<tr>
+
+<td><?php echo $user['id']; ?></td>
+<td><?php echo $user['username']; ?></td>
+<td><?php echo $user['email']; ?></td>
+
+<td>
+
+<a href="edit_user.php?id=<?php echo $user['id']; ?>">Sửa</a>
+
+<form action="delete_user.php" method="POST" style="display:inline;">
+
+<input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+
+<input type="hidden" name="csrf_token"
+value="<?php echo csrf_token(); ?>">
+
+<button onclick="return confirm('Bạn có chắc muốn xóa?')">
+Xóa
+</button>
+
+</form>
+
+</td>
+
+<td>
+<a href="user_detail.php?id=<?php echo $user['id']; ?>">
+Chi tiết
+</a>
+</td>
+
+</tr>
 <?php endforeach; ?>
 
 </table>
